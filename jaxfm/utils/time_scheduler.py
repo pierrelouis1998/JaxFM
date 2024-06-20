b9d46c18b749f32ebee4dac496bd7bc8acb8bce3
pruhlman <pierre-louis.ruhlmann@inria.fr>
2024-06-20 14:46:12 +0200

    Add doc and typing

--- jaxfm/utils/time_scheduler.py
+++ jaxfm/utils/time_scheduler.py
@@ -1,8 +1,25 @@
-import jax.numpy as jnp
 import jax
+import jax.numpy as jnp
+from jaxtyping import Array, Float
 
-def get_t_samples(key: int, num_samples: int, law: str = "uniform", **kwargs) -> jnp.ndarray:
-    """Sample the t values"""
+
+def get_t_samples(
+    key: int, num_samples: int, law: str = "uniform", **kwargs
+) -> Float[Array, "num_samples 1"]:
+    """Sample the t values for a given law
+
+    Args:
+        key: Random key
+        num_samples: Number of samples
+        law: Law to sample from
+        **kwargs: Optional arguments for the law
+
+    Raises:
+        ValueError: If the law is unknown
+
+    Returns:
+        Float[Array, "num_samples 1"]: The sampled t values
+    """
     if law == "normal":
         t = 0.3 * jax.random.normal(key, (num_samples, 1)) + 0.5
         # rescale to [0, 1]
@@ -17,5 +34,3 @@ def get_t_samples(key: int, num_samples: int, law: str = "uniform", **kwargs) ->
         raise ValueError(f"Unknown law {law}")
 
     return t
-
-
